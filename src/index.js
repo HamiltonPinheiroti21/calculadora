87578fd9f5c81963f9539d79b8d5deec808d609b
Hamilton <hamiltonpinheiroti21@gmail.com>
2020-05-30 04:46:46 -0300

    commit projeto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Caclulator from './main/caclulator';
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
     <App />
-  </React.StrictMode>,
+    <h1>Calculadora</h1>
+    <h1>React</h1>
+    <Caclulator />
+  </div>,
   document.getElementById('root')
 );
 
