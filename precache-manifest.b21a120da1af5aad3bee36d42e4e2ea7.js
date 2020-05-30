1b4dac996d75f10022b1d3ca381285855dcf1fac
Hamilton <hamiltonpinheiroti21@gmail.com>
2020-05-30 06:56:55 -0300

    Updates

---
+++ precache-manifest.b21a120da1af5aad3bee36d42e4e2ea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a45d8bcc193331a4def39f60cb5ef6",
+    "url": "/calculadora/index.html"
+  },
+  {
+    "revision": "7e4228f2ce32705d5e19",
+    "url": "/calculadora/static/css/main.c108c1cd.chunk.css"
+  },
+  {
+    "revision": "f385ba6418a087fbcdf9",
+    "url": "/calculadora/static/js/2.8e68a0bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculadora/static/js/2.8e68a0bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4228f2ce32705d5e19",
+    "url": "/calculadora/static/js/main.307fe7d4.chunk.js"
+  },
+  {
+    "revision": "5287421a3945ccfe53a8",
+    "url": "/calculadora/static/js/runtime-main.e793c175.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculadora/static/media/logo.5d5d9eef.svg"
+  }
+]);
